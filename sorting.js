1e4b4735379cdf92c2bed3476816db810dd6bd3e
Bryan Dewa Wicaksana <bryan.wicaksanaa@gmail.com>
2023-08-23 18:16:53 +0700

    bring back removed test case hehe

--- sorting.js
+++ sorting.js
@@ -19,8 +19,8 @@ const groupAnagrams = function(strs) {
     return arr;
   }
   for (let i = 0; i < strs.length; i++) {
-    const sorted = bubbleSort([...strs[i]]);
-    const sortedStr = sorted.join('');
+    sorted = bubbleSort([...strs[i]]);
+    sortedStr = sorted.join('');
     if (memo[sortedStr] === undefined) {
       memo[sortedStr] = result.length;
       result[memo[sortedStr]] = [strs[i]];
@@ -30,3 +30,31 @@ const groupAnagrams = function(strs) {
   }
   return result;
 };
+
+// Test Case 1
+console.log(groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"])); 
+// Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+
+// Test Case 2
+console.log(groupAnagrams([""])); 
+// Output: [[""]]
+
+// Test Case 3
+console.log(groupAnagrams(["a"])); 
+Output: [["a"]]
+
+// Test Case 4
+console.log(groupAnagrams(["listen", "silent", "hello", "world"])); 
+// Output: [["listen","silent"],["hello"],["world"]]
+
+// Test Case 5
+console.log(groupAnagrams(["rat", "tar", "art", "car"])); 
+// Output: [["rat","tar","art"],["car"]]
+
+// Test Case 6
+console.log(groupAnagrams(["apple", "banana", "leapp", "grape", "orange"])); 
+// Output: [["apple","leapp"],["banana"],["grape"],["orange"]]
+
+// Test Case 7
+console.log(groupAnagrams(["abcd", "dcba", "xyz", "zyx", "wxyz"])); 
+// Output: [["abcd","dcba"],["xyz","zyx"],["wxyz"]]
