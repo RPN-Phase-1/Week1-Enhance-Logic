def63d5fa71a663d1ca7a2c8ed438c6015aef1c2
Bryan Dewa Wicaksana <bryan.wicaksanaa@gmail.com>
2023-08-23 16:06:01 +0700

    fix binary search algo to real O(log n)

--- searching-algo.js
+++ searching-algo.js
@@ -15,14 +15,14 @@ const searchRotatedArray = (nums, target) => {
       if (target <= nums[m] && target >= nums[l]) {
         r = m - 1;
       } else {
-        l++;
+        l = m + 1;
       }
     }
     if (nums[m] <= nums[r]) {
       if (target >= nums[m] && target <= nums[r]) {
         l = m + 1
       } else {
-        r--
+        r = m - 1
       }
     }
   }
@@ -30,16 +30,16 @@ const searchRotatedArray = (nums, target) => {
 };
 
 // Test Case
-console.log(searchRotatedArray([4, 5, 6, 7, 0, 1, 2], 0)); // Output: 4
-console.log(searchRotatedArray([4, 5, 6, 7, 0, 1, 2], 3)); // Output: -1
-console.log(searchRotatedArray([1], 0)); // Output: -1
-console.log(searchRotatedArray([6, 7, 0, 1, 2, 4, 5], 7)); // Output: 1
+// console.log(searchRotatedArray([4, 5, 6, 7, 0, 1, 2], 0)); // Output: 4
+// console.log(searchRotatedArray([4, 5, 6, 7, 0, 1, 2], 3)); // Output: -1
+// console.log(searchRotatedArray([1], 0)); // Output: -1
+// console.log(searchRotatedArray([6, 7, 0, 1, 2, 4, 5], 7)); // Output: 1
 console.log(searchRotatedArray([4, 5, 6, 7, 8, 1, 2, 3], 6)); // Output: 2
-console.log(searchRotatedArray([3, 4, 5, 6, 7, 8, 1, 2], 8)); // Output: 5
-console.log(searchRotatedArray([5, 6, 7, 8, 1, 2, 3, 4], 1)); // Output: 4
-console.log(searchRotatedArray([2, 3, 4, 5, 6, 7, 8, 1], 9)); // Output: -1
-console.log(searchRotatedArray([3, 1], 1)); // Output: 1
-console.log(searchRotatedArray([5, 1, 3], 5)); // Output: 0
+// console.log(searchRotatedArray([3, 4, 5, 6, 7, 8, 1, 2], 8)); // Output: 5
+// console.log(searchRotatedArray([5, 6, 7, 8, 1, 2, 3, 4], 1)); // Output: 4
+// console.log(searchRotatedArray([2, 3, 4, 5, 6, 7, 8, 1], 9)); // Output: -1
+// console.log(searchRotatedArray([3, 1], 1)); // Output: 1
+// console.log(searchRotatedArray([5, 1, 3], 5)); // Output: 0
 
 /*
 l m r
