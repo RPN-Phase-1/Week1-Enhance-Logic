a0860448897a7962b3d682cccfacfebf9016731b
Bryan Dewa Wicaksana <bryan.wicaksanaa@gmail.com>
2023-08-26 18:48:23 +0700

    dijkstra selesai bersama semua anak phase 1 wkwkwk

--- test.js
+++ test.js
@@ -1,74 +1,85 @@
-class Graph
-{
-     
-    // Constructor
-    constructor(v)
-    {
-        this.V = v;
-        this.adj = new Array(v);
-        for(let i = 0; i < v; i++)
-            this.adj[i] = [];
-    }
-     
-    // Function to add an edge into the graph
-    addEdge(v, w)
-    {
-         
-        // Add w to v's list.
-        this.adj[v].push(w);
-    }
-    // A function used by DFS
-    DFSUtil(v, visited)
-    {
-         
-        // Mark the current node as visited and print it
-        visited[v] = true;
-        console.log(v);
+function shortestPathWeightedGraph(graph, start, target) {
+    const INF = Number.MAX_SAFE_INTEGER; // Jarak tak terhingga untuk inisialisasi
+    const distances = {}; // Menyimpan jarak terpendek dari start ke setiap simpul
+    const visited = new Set(); // Menandai simpul yang sudah dikunjungi
+    const priorityQueue = []; // Priority queue sederhana
   
-        // Recur for all the vertices adjacent to this
-        // vertex
-        for(let i of this.adj[v].values())
-        {
-            let n = i
-            if (!visited[n])
-                this.DFSUtil(n, visited);
+    // Inisialisasi jarak dari start ke semua simpul dengan tak terhingga
+    for (const node in graph) {
+      distances[node] = INF;
+    }
+  
+    distances[start] = 0; // Jarak dari start ke start adalah 0
+    priorityQueue.push([start, 0]); // Masukkan simpul start ke priority queue dengan jarak 0
+  
+    // Loop hingga priority queue kosong
+    while (priorityQueue.length > 0) {
+      priorityQueue.sort((a, b) => a[1] - b[1]); // Urutkan berdasarkan jarak terpendek
+      const [currentNode, currentDistance] = priorityQueue.shift(); // Ambil simpul dengan jarak terpendek dari priority queue
+  
+      if (visited.has(currentNode)) {
+        continue; // Lewati jika simpul sudah dikunjungi sebelumnya
+      }
+  
+      visited.add(currentNode); // Tandai simpul sebagai dikunjungi
+  
+      // Iterasi melalui tetangga dari simpul saat ini
+      for (const { node: neighbor, weight } of graph[currentNode]) {
+        const totalDistance = currentDistance + weight;
+        // Update jarak terpendek jika totalDistance lebih kecil dari jarak sebelumnya
+        if (totalDistance < distances[neighbor]) {
+          distances[neighbor] = totalDistance;
+          priorityQueue.push([neighbor, totalDistance]); // Masukkan simpul tetangga ke priority queue
         }
+      }
     }
-     
-    // The function to do DFS traversal.
-    // It uses recursive
-    // DFSUtil()
-    DFS(v)
-    {
-         
-        // Mark all the vertices as
-        // not visited(set as
-        // false by default in java)
-        console.log(this.adj)
-        let visited = new Array(this.V);
-        for(let i = 0; i < this.V; i++)
-            visited[i] = false;
   
-        // Call the recursive helper
-        // function to print DFS
-        // traversal
-        this.DFSUtil(v, visited);
-    }
-}
- 
-// Driver Code
-g = new Graph(4);
+    // Kembalikan jarak terpendek dari start ke target, jika tidak ada jalur, kembalikan -1
+    return distances[target] !== INF ? distances[target] : -1;
+  }
   
-g.addEdge(0, 1);
-g.addEdge(0, 2);
-g.addEdge(1, 2);
-g.addEdge(2, 0);
-g.addEdge(2, 3);
-g.addEdge(3, 3);
- 
-console.log("Following is Depth First Traversal " +
-               "(starting from vertex 2)");
- 
-g.DFS(2);
- 
-// This code is contributed by avanitrachhadiya2155
+  // Implementasi Dijkstra's Algorithm dimulai dari fungsi shortestPathWeightedGraph
+  // yang menerima graph berbobot, simpul awal, dan simpul tujuan. Kode tersebut
+  // melakukan inisialisasi variabel dan priority queue, lalu iterasi melalui simpul-simpul
+  // untuk mencari jarak terpendek dari start ke target.
+  // Setelah selesai, fungsi mengembalikan jarak terpendek atau -1 jika tidak ada jalur.
+  
+  // Testcase 1
+  console.log(shortestPathWeightedGraph({
+    // Visualisasi graph:
+    //   A --3-- B --1-- C --2-- D
+    'A': [{ node: 'B', weight: 3 }, { node: 'C', weight: 2 }],
+    'B': [{ node: 'C', weight: 1 }, { node: 'D', weight: 4 }],
+    'C': [{ node: 'D', weight: 2 }],
+    'D': []
+  }, 'A', 'D')); // Output: 4
+  
+  // Testcase 2
+  console.log(shortestPathWeightedGraph({
+    // Visualisasi graph:
+    //   A --3-- B --1-- C --2-- D
+    'A': [{ node: 'B', weight: 3 }, { node: 'C', weight: 2 }],
+    'B': [{ node: 'C', weight: 1 }, { node: 'D', weight: 4 }],
+    'C': [{ node: 'D', weight: 2 }],
+    'D': []
+  }, 'B', 'D')); // Output: 3
+  
+  // Testcase 3
+  console.log(shortestPathWeightedGraph({
+    // Visualisasi graph:
+    //   A --3-- B --1-- C --2-- D
+    'A': [{ node: 'B', weight: 3 }, { node: 'C', weight: 2 }],
+    'B': [{ node: 'C', weight: 1 }, { node: 'D', weight: 4 }],
+    'C': [{ node: 'D', weight: 2 }],
+    'D': []
+  }, 'A', 'A')); // Output: 0
+  
+  // Testcase 4
+  console.log(shortestPathWeightedGraph({
+    // Visualisasi graph:
+    //   A --3-- B --1-- C --2-- D
+    'A': [{ node: 'B', weight: 3 }, { node: 'C', weight: 2 }],
+    'B': [{ node: 'C', weight: 1 }, { node: 'D', weight: 4 }],
+    'C': [{ node: 'D', weight: 2 }],
+    'D': []
+  }, 'C', 'B')); // Output: -1
