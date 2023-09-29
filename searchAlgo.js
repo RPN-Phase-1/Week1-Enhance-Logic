3c9666ec4d83a103ca6df952be33a20765650ebe
Arya Enrico <aryaenrico789@gmail.com>
2023-09-29 19:10:42 +0700

    Fix Searching Algorihtm Part 4

--- searchAlgo.js
+++ searchAlgo.js
@@ -5,24 +5,27 @@ const searchRotatedArray = (nums, target) => {
 
   while (start <= end) {
     let mid = parseInt((start + end) / 2);
-
     if (nums[mid] == target) {
       return mid;
     } else {
-      if (nums[mid] > target) {
-        if (nums[start] > target) {
-          start = mid + 1;
-        } else {
-          end = mid - 1;
-        }
-      } else {
-        if (nums[start] >= target) {
-          end = mid - 1;
-        } else if (nums[end] <= target) {
-          if (nums[start] > nums[end]) {
-            start++;
+      // urut sisi kanan
+      if (nums[start] <= nums[mid]) {
+        if (target >= nums[start]) {
+          if (target < nums[mid]) {
+            end = mid - 1;
           } else {
             start = mid + 1;
+          }
+        } else {
+          start = mid + 1;
+        }
+      } else {
+        // urut sisi kiri
+        if (nums[end] >= nums[mid]) {
+          if (nums[end] >= target && target >= nums[mid]) {
+            start = mid + 1;
+          } else {
+            end = mid - 1;
           }
         }
       }
@@ -31,14 +34,4 @@ const searchRotatedArray = (nums, target) => {
   return -1;
 };
 
-// Test Case
-console.log(searchRotatedArray([4, 5, 6, 7, 0, 1, 2], 0)); // Output: 4
-console.log(searchRotatedArray([4, 5, 6, 7, 0, 1, 2], 3)); // Output: -1
-console.log(searchRotatedArray([1], 0)); // Output: -1
-console.log(searchRotatedArray([6, 7, 0, 1, 2, 4, 5], 7)); // Output: 1
-console.log(searchRotatedArray([4, 5, 6, 7, 8, 1, 2, 3], 6)); // Output: 2
-console.log(searchRotatedArray([3, 4, 5, 6, 7, 8, 1, 2], 8)); // Output: 5
-console.log(searchRotatedArray([5, 6, 7, 8, 1, 2, 3, 4], 1)); // Output: 4
-console.log(searchRotatedArray([2, 3, 4, 5, 6, 7, 8, 1], 9)); // Output: -1
-console.log(searchRotatedArray([3, 1], 1)); // Output: 1
-console.log(searchRotatedArray([5, 1, 3], 5)); // Output: 0
+console.log(searchRotatedArray([6, 7, 0, 1, 2, 4, 5], 5));
