ec4f489f43c0bcc28bbac8791ba2e6ecdda9e14a
Yogi Wijaya <micornos@gmail.com>
2023-10-19 13:33:10 +0700

    Submit Logic Nolep OOP

---
+++ Week-1/OOP-part2/soal3.js
@@ -0,0 +1,17 @@
+class Student {
+    constructor(name, age, grade){
+        this._name = name
+        this._age = age
+        this._grade = grade
+    }
+    getInfo(){
+        if (this._age < 6 || this._age > 18 || this._grade < 0 || this._grade > 100) return `Invalid Age or Grade `
+        return `Name: ${this._name}, Age: ${this._age}, Grade ${this._grade}`
+    }
+}
+
+const student1 = new Student('Alice', 12, 85);
+console.log(student1.getInfo()); // Output: Name: Alice, Age: 12, Grade: 85
+
+const student2 = new Student('Bob', 20, 95);
+console.log(student2.getInfo()); // Output: Invalid Age or Grade
