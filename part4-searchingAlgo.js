95ca866e039efe8f717d23a506cc4b6b4a8688b5
Yogi Wijaya <micornos@gmail.com>
2023-10-22 16:22:11 +0700

    Submit graph.js

--- part4-searchingAlgo.js
+++ part4-searchingAlgo.js
@@ -8,10 +8,8 @@ const searchRotatedArray = (nums, target) => {
     else if (nums[left] <= nums[mid]) {
       if (target >= nums[left] && target < nums[mid]) right = mid - 1;
       else left = mid + 1;
-    } else {
-      if (target <= nums[right] && target > nums[mid]) left = mid + 1;
-      else right = mid - 1;
-    }
+    } else if (target <= nums[right] && target > nums[mid]) left = mid + 1;
+    else right = mid - 1;
   }
   return -1;
 };
