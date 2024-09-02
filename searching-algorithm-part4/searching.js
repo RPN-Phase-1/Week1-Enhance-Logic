5ec1044dffaa22046abe52fc093f73d62df920c2
murdock33 <tomhagen849@gmail.com>
2024-09-02 19:28:31 +0700

    push ulang logic nolep searchAlgo.js, kode sebelumnya bug

--- searching-algorithm-part4/searching.js
+++ searching-algorithm-part4/searching.js
@@ -1,26 +1,31 @@
 const searchRotatedArray = (nums, target) => {
   //code
-  let left = 0;
-  let right = nums.length - 1;
+  let left = 0,
+    right = nums.length - 1;
 
   while (left <= right) {
     let mid = Math.floor((left + right) / 2);
+
     if (nums[mid] === target) {
-    } else if (nums[mid] <= nums[right]) {
-      if (target > nums[mid] && target <= nums[right]) {
-        left = mid + 1;
-      } else {
-        right = mid - 1;
-      }
-    } else {
+      return mid;
+    }
+
+    if (nums[left] <= nums[mid]) {
       if (target >= nums[left] && target < nums[mid]) {
         right = mid - 1;
       } else {
         left = mid + 1;
       }
+    } else {
+      if (target > nums[mid] && target <= nums[right]) {
+        left = mid + 1;
+      } else {
+        right = mid - 1;
+      }
     }
   }
-  return -1; // Mengembalikan -1 jika elemen tidak ditemukan
+
+  return -1;
 };
 
 // Test Case
