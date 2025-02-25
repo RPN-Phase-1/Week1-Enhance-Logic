5ad1f2c9d00c003eba5106bc5123a95bd9b1d682
Eunbimz <timeforbima123@gmail.com>
2025-02-25 11:02:48 +0700

    in-djikstra

--- logic_nolep/in-sortingAlgo.js
+++ logic_nolep/in-sortingAlgo.js
@@ -1,103 +1,120 @@
-function bubbleSort(strs){
-    n = strs.length;
-    let huruf = strs.split("");
-    
-    for(let i = 0; i < n - 1; i++){
-        for(let j = 0; j < n - i - 1; j++){
-            if (huruf[j] > huruf[j + 1]) {
-                let temp = huruf[j];
-                huruf[j] = huruf[j + 1];
-                huruf[j + 1] = temp;
-            }        
-        }
-    }
-    return huruf.join("");
-}
-
-function selectionSort(strs) {
-    let n = strs.length;
-    let huruf = strs.split("");
-    
-    for (let i = 0; i < n - 1; i++) {
-        let minIndex = i;
+// Implementasi bubble sort
+class bubble_sort{
+    bubbleSort(strs){
+        let n = strs.length;
+        let huruf = strs.split("");
         
-        for (let j = i + 1; j < n; j++) {
-            if (huruf[j] < huruf[minIndex]) {
-                minIndex = j;
+        for(let i = 0; i < n - 1; i++){
+            for(let j = 0; j < n - i - 1; j++){
+                if (huruf[j] > huruf[j + 1]) {
+                    let temp = huruf[j];
+                    huruf[j] = huruf[j + 1];
+                    huruf[j + 1] = temp;
+                }        
             }
         }
-        
-        let temp = huruf[i];
-        huruf[i] = huruf[minIndex];
-        huruf[minIndex] = temp;
+        return huruf.join("");
     }
-    return huruf.join("");
 }
 
-function insertionSort(strs) {
-    let n = strs.length;
-    let huruf = strs.split("");
-    
-    for (let i = 1; i < n; i++) {
-        let current = huruf[i];
-        let j = i - 1;
+// Implementasi selection sort
+class selection_sort{
+    selectionSort(strs) {
+        let n = strs.length;
+        let huruf = strs.split("");
         
-        while (j >= 0 && huruf[j] > current) {
-            huruf[j + 1] = huruf[j];
-            j--;
+        for (let i = 0; i < n - 1; i++) {
+            let minIndex = i;
+            
+            for (let j = i + 1; j < n; j++) {
+                if (huruf[j] < huruf[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            
+            let temp = huruf[i];
+            huruf[i] = huruf[minIndex];
+            huruf[minIndex] = temp;
+        }
+        return huruf.join("");
+    }
+}
+
+// Implementasi insertion sort
+class insertion_sort{
+    insertionSort(strs) {
+        let n = strs.length;
+        let huruf = strs.split("");
+        
+        for (let i = 1; i < n; i++) {
+            let current = huruf[i];
+            let j = i - 1;
+            
+            while (j >= 0 && huruf[j] > current) {
+                huruf[j + 1] = huruf[j];
+                j--;
+            }
+            
+            huruf[j + 1] = current;
+        }
+        return huruf.join("");
+    }
+}
+
+// Implementasi merge sort
+class merge_sort{
+    mergeSort(strs) {
+        let huruf = strs.split("");
+        if (huruf.length <= 1) {
+            return strs;
         }
         
-        huruf[j + 1] = current;
-    }
-    return huruf.join("");
-}
-
-function mergeSort(strs) {
-    let huruf = strs.split("");
-    if (huruf.length <= 1) {
-        return strs;
+        const middle = Math.floor(huruf.length / 2);
+        const left = huruf.slice(0, middle);
+        const right = huruf.slice(middle);
+        
+        const sortedLeft = this.mergeSort(left.join(""));
+        const sortedRight = this.mergeSort(right.join(""));
+        
+        return this.merge(sortedLeft.split(""), sortedRight.split("")).join("");
     }
     
-    const middle = Math.floor(huruf.length / 2);
-    const left = huruf.slice(0, middle);
-    const right = huruf.slice(middle);
-    
-    const sortedLeft = mergeSort(left.join(""));
-    const sortedRight = mergeSort(right.join(""));
-    
-    return merge(sortedLeft.split(""), sortedRight.split("")).join("");
-}
-  
-function merge(left, right) {
-    let result = [];
-    let leftIndex = 0;
-    let rightIndex = 0;
-  
-    while (leftIndex < left.length && rightIndex < right.length) {
-      if (left[leftIndex] < right[rightIndex]) {
-        result.push(left[leftIndex]);
-        leftIndex++;
-      } else {
-        result.push(right[rightIndex]);
-        rightIndex++;
-      }
+    merge(left, right) {
+        let result = [];
+        let leftIndex = 0;
+        let rightIndex = 0;
+      
+        while (leftIndex < left.length && rightIndex < right.length) {
+          if (left[leftIndex] < right[rightIndex]) {
+            result.push(left[leftIndex]);
+            leftIndex++;
+          } else {
+            result.push(right[rightIndex]);
+            rightIndex++;
+          }
+        }
+      
+        return result.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
     }
-  
-    return result.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
 }
 
 const groupAnagrams = function(strs) {
     let ubah = new Map();
 
+    let bubble = new bubble_sort();
+    let selection = new selection_sort();
+    let insertion = new insertion_sort();
+    let merge = new merge_sort();
+
     strs.forEach(kata => {
-        let key = mergeSort(kata); 
+        let key = bubble.bubbleSort(kata); 
         if (!ubah.has(key)) {
             ubah.set(key, []);
         }
         ubah.get(key).push(kata);
     });
     return Array.from(ubah.values());
-};
+}
     
     // Test Case 1
     console.log(groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"])); 
